bbdc35c40dbc7756ec7095f46d94f4143c9a2fe3
Urasaki <1164688204@qq.com>
2020-12-30 18:34:15 +0800

    Update MVVM module

--- app/src/main/java/com/mobile/rxjava2andretrofit2/kotlin/project/ImageLoad.kt
+++ app/src/main/java/com/mobile/rxjava2andretrofit2/kotlin/project/ImageLoad.kt
@@ -5,13 +5,11 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 
 
-class ImageLoad {
+object ImageLoad {
 
-    companion object {
-        @JvmStatic
-        @BindingAdapter("bind:imageUrl")
-        fun bindImage(imageView: ImageView, url: String) {
-            Glide.with(imageView.getContext()).load(url).into(imageView)
-        }
+    @JvmStatic
+    @BindingAdapter("bind:imageUrl")
+    fun bindImage(imageView: ImageView, url: String) {
+        Glide.with(imageView.getContext()).load(url).into(imageView)
     }
 }
