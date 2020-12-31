1446221eaf7e2dc035127ba7a8c9c8e04f5f257e
Urasaki <1164688204@qq.com>
2020-12-31 17:35:18 +0800

    Update MVVM module

--- app/src/main/java/com/mobile/rxjava2andretrofit2/kotlin/project/ImageLoad.kt
+++ app/src/main/java/com/mobile/rxjava2andretrofit2/kotlin/project/ImageLoad.kt
@@ -11,7 +11,7 @@ class ImageLoad {
         @JvmStatic
         @BindingAdapter("bind:imageUrl")
         fun bindImage(imageView: ImageView, url: String) {
-            Glide.with(imageView.getContext()).load(url).into(imageView)
+            Glide.with(imageView.context).load(url).into(imageView)
         }
     }
 }
