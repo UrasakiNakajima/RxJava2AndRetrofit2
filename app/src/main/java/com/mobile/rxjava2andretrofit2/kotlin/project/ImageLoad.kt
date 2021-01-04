92b03ff44fe0f91760b4ef172152da00f79c38ac
Urasaki <1164688204@qq.com>
2021-01-04 11:56:15 +0800

    Update

--- app/src/main/java/com/mobile/rxjava2andretrofit2/kotlin/project/ImageLoad.kt
+++ app/src/main/java/com/mobile/rxjava2andretrofit2/kotlin/project/ImageLoad.kt
@@ -1,17 +1,64 @@
 package com.mobile.rxjava2andretrofit2.kotlin.project
 
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.ImageViewTarget
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
+import com.mobile.rxjava2andretrofit2.manager.LogManager
 
 
 class ImageLoad {
 
+
     companion object {
+
+        val TAG = "ImageLoad";
+
         @JvmStatic
         @BindingAdapter("bind:imageUrl")
         fun bindImage(imageView: ImageView, url: String) {
             Glide.with(imageView.context).load(url).into(imageView)
+            Glide.with(imageView.context).load(url)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            LogManager.i(TAG, "网络访问失败，请检查是否开始网络或者增加http的访问许可");
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            LogManager.i(TAG, "网络访问成功，可以显示图片");
+                            return false
+                        }
+
+                    })
+                    .into(object : ImageViewTarget<Drawable>(imageView) {
+
+                        override fun onLoadStarted(placeholder: Drawable?) {
+                            super.onLoadStarted(placeholder)
+                            LogManager.i(TAG, "图片开始加载");
+                        }
+
+                        override fun onLoadFailed(errorDrawable: Drawable?) {
+                            super.onLoadFailed(errorDrawable)
+                            LogManager.i(TAG, "图片加载失败");
+                        }
+
+                        override fun onResourceReady(resource: Drawable, transition: Transition<in Drawable>?) {
+                            super.onResourceReady(resource, transition)
+                            LogManager.i(TAG, "图片加载完成");
+                            imageView.setImageDrawable(resource)
+                        }
+
+                        override fun setResource(resource: Drawable?) {
+                            LogManager.i(TAG, "设置资源");
+                        }
+                    })
         }
     }
 }
