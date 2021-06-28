2d980c753395eec2cea93ef63b19fc96b394460c
Urasaki <1164688204@qq.com>
2021-06-28 11:11:49 +0800

    update AIDL function

--- aidl_client/src/main/aidl/com/mobile/aidl_client/Book.java
+++ aidl_client/src/main/aidl/com/mobile/aidl_client/Book.java
@@ -4,59 +4,64 @@ import android.os.Parcel;
 import android.os.Parcelable;
 
 public class Book implements Parcelable {
-
+    
     private String name;
-    private int price;
-
+    private String content;
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
-    public int getPrice() {
-        return price;
+    
+    public String getContent() {
+        return content;
     }
-
-    public void setPrice(int price) {
-        this.price = price;
+    
+    public void setContent(String content) {
+        this.content = content;
     }
-
+    
     @Override
     public String toString() {
         return "Book{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                '}';
+                   "name='" + name + '\'' +
+                   ", content='" + content + '\'' +
+                   '}';
     }
-
+    
     @Override
     public int describeContents() {
         return 0;
     }
-
+    
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeString(this.name);
-        dest.writeInt(this.price);
+        dest.writeString(this.content);
     }
-
+    
+    public void readFromParcel(Parcel source) {
+        this.name = source.readString();
+        this.content = source.readString();
+    }
+    
     public Book() {
     }
-
+    
     protected Book(Parcel in) {
         this.name = in.readString();
-        this.price = in.readInt();
+        this.content = in.readString();
     }
-
+    
     public static final Creator<Book> CREATOR = new Creator<Book>() {
         @Override
         public Book createFromParcel(Parcel source) {
             return new Book(source);
         }
-
+        
         @Override
         public Book[] newArray(int size) {
             return new Book[size];
--- aidl_server/src/main/java/com/mobile/aidl_server/service/AidlService.java
+++ aidl_server/src/main/java/com/mobile/aidl_server/service/AidlService.java
@@ -13,60 +13,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AidlService extends Service {
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Book book = new Book();
-        book.setName("Android开发艺术探索");
-        book.setPrice(28);
-        mBooks.add(book);
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.i(getClass().getSimpleName(), String.format("on bind,intent = %s", intent.toString()));
-        return mBookManager;
-    }
-
-    public final String TAG = this.getClass().getSimpleName();
-
-    //包含Book对象的list
-    private List<Book> mBooks = new ArrayList<>();
-
-    //由AIDL文件生成的BookManager
-    private final BookManager.Stub mBookManager = new BookManager.Stub() {
-        @Override
-        public List<Book> getBooks() throws RemoteException {
-            synchronized (this) {
-                Log.i(TAG, "invoking getBooks() method , now the list is : " + mBooks.toString());
-                if (mBooks != null) {
-                    return mBooks;
-                }
-                return new ArrayList<>();
-            }
-        }
-
-
-        @Override
-        public void addBook(Book book) throws RemoteException {
-            synchronized (this) {
-                if (mBooks == null) {
-                    mBooks = new ArrayList<>();
-                }
-                if (book == null) {
-                    Log.i(TAG, "Book is null in In");
-                    book = new Book();
-                }
-                //尝试修改book的参数，主要是为了观察其到客户端的反馈
-                book.setPrice(2333);
-                if (!mBooks.contains(book)) {
-                    mBooks.add(book);
-                }
-                //打印mBooks列表，观察客户端传过来的值
-                Log.i(TAG, "invoking addBooks() method , now the list is : " + mBooks.toString());
-            }
-        }
-    };
-
+	
+	private static final String TAG = "AidlService";
+	
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Book book = new Book();
+		book.setName("Android AIDL 测试通过");
+		book.setContent("Android AIDL内容");
+		mBooks.add(book);
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.i(TAG, String.format("on bind,intent = %s", intent.toString()));
+		return mBookManager;
+	}
+	
+	//包含Book对象的list
+	private List<Book> mBooks = new ArrayList<>();
+	
+	//由AIDL文件生成的BookManager
+	private final BookManager.Stub mBookManager = new BookManager.Stub() {
+		@Override
+		public List<Book> getBooks() throws RemoteException {
+			synchronized (this) {
+				Log.i(TAG, "invoking getBooks() method , now the list is : " + mBooks.toString());
+				if (mBooks != null) {
+					return mBooks;
+				}
+				return new ArrayList<>();
+			}
+		}
+		
+		@Override
+		public void addBook(Book book) throws RemoteException {
+			synchronized (this) {
+				if (mBooks == null) {
+					mBooks = new ArrayList<>();
+				}
+				if (book == null) {
+					Log.i(TAG, "Book is null in In");
+					book = new Book();
+				}
+				//尝试修改book的参数，主要是为了观察其到客户端的反馈
+				book.setContent("Android AIDL内容");
+				if (!mBooks.contains(book)) {
+					mBooks.add(book);
+				}
+				//打印mBooks列表，观察客户端传过来的值
+				Log.i(TAG, "invoking addBooks() method , now the list is : " + mBooks.toString());
+			}
+		}
+	};
+	
 }
