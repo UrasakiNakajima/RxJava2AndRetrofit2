c1ffff165e10831c917d21996b8efddfd14a752e
“Urasaki“ <1164688204@qq.com>
2021-07-19 22:03:02 +0800

    Update

--- square_module/src/main/java/com/mobile/square_module/SquareFragment.kt
+++ square_module/src/main/java/com/mobile/square_module/SquareFragment.kt
@@ -46,7 +46,6 @@ class SquareFragment() : BaseMvvmFragment<SquareViewModelImpl, FragmentSquareBin
         mDatabind.datax = datax
 
         mDatabind.executePendingBindings()
-
     }
 
     override fun initObservers() {
