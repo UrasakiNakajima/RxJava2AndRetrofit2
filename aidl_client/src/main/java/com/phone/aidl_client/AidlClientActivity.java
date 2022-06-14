275eeadec0c80b4ccd09c5035d108ee28384caa5
“Urasaki“ <1164688204@qq.com>
2022-06-14 22:27:31 +0800

    Update 1.修改aidl功能。

--- aidl_client/src/main/java/com/phone/aidl_client/AidlClientActivity.java
+++ aidl_client/src/main/java/com/phone/aidl_client/AidlClientActivity.java
@@ -7,7 +7,6 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,139 +20,139 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AidlClientActivity extends BaseRxAppActivity {
-	
-	private static final String       TAG = "AidlClientActivity";
-	private              TextView     tevBookName;
-	private              TextView     tevBookContent;
-	private              RecyclerView rcvBook;
-	private              TextView     tevAddBook;
-	
-	//由AIDL文件生成的Java类
-	private BookManager mBookManager = null;
-	
-	//标志当前与服务端连接状况的布尔值，false为未连接，true为连接中
-	private boolean isConnectServer = false;
-	
-	//包含Book对象的list
-	private List<Book> mBookList = new ArrayList<>();
-	private int        num;
-	
-	private LinearLayoutManager mLayoutManager;
-	private BookAdapter         mBookAdapter;
-	
-	@Override
-	protected int initLayoutId() {
-		return R.layout.activity_aidl_client;
-	}
-	
-	@Override
-	protected void initData() {
-		num = 0;
-	}
-	
-	@Override
-	protected void initViews() {
-		tevBookName = (TextView) findViewById(R.id.tev_book_name);
-		tevBookContent = (TextView) findViewById(R.id.tev_book_content);
-		rcvBook = (RecyclerView) findViewById(R.id.rcv_book);
-		tevAddBook = (TextView) findViewById(R.id.tev_add_book);
-		
-		tevAddBook.setOnClickListener(view -> {
-			
-			initAddBook();
-		});
-		
-		initAdapter();
-	}
-	
-	private void initAdapter() {
-		mLayoutManager = new LinearLayoutManager(rxAppCompatActivity);
-		mLayoutManager.setOrientation(RecyclerView.VERTICAL);
-		rcvBook.setLayoutManager(mLayoutManager);
-		rcvBook.setItemAnimator(new DefaultItemAnimator());
-		
-		mBookAdapter = new BookAdapter(rxAppCompatActivity);
-		rcvBook.setAdapter(mBookAdapter);
-	}
-	
-	private void initAddBook() {
-		//如果与服务端的连接处于未连接状态，则尝试连接
-		if (!isConnectServer) {
-			attemptToBindService();
-			Toast.makeText(this, "当前与服务端处于未连接状态，正在尝试重连，请稍后再试", Toast.LENGTH_SHORT).show();
-			return;
-		}
-		if (mBookManager == null) {
-			return;
-		}
-		
-		num++;
-		Book book = new Book();
-		book.setName("新书" + num);
-		book.setContent("新书内容" + num);
-		try {
-			mBookManager.addBook(book);
-			mBookList.clear();
-			mBookList.addAll(mBookManager.getBookList());
-			mBookAdapter.clearData();
-			mBookAdapter.addAllData(mBookList);
-			LogManager.i(TAG, "initAddBook*****" + book.toString());
-		} catch (RemoteException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	protected void initLoadData() {
-	
-	}
-	
-	/**
-	 * 尝试与服务端建立连接
-	 */
-	private void attemptToBindService() {
-		Intent intent = new Intent();
-		intent.setAction("com.phone.aidl_client.aidl");
-		intent.setPackage("com.phone.aidl_server");
-		bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
-	}
-	
-	private ServiceConnection mServiceConnection = new ServiceConnection() {
-		@Override
-		public void onServiceConnected(ComponentName name, IBinder service) {
-			LogManager.i(TAG, "service connected");
-			mBookManager = BookManager.Stub.asInterface(service);
-			isConnectServer = true;
-			
-			if (mBookManager != null) {
-				if (mBookList.size() > 0) {
-					LogManager.i(TAG, "onServiceConnected*****" + mBookList.toString());
-				}
-			}
-		}
-		
-		@Override
-		public void onServiceDisconnected(ComponentName name) {
-			LogManager.i(TAG, "service disconnected");
-			isConnectServer = false;
-		}
-	};
-	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		if (!isConnectServer) {
-			attemptToBindService();
-		}
-	}
-	
-	@Override
-	protected void onStop() {
-		super.onStop();
-		if (isConnectServer) {
-			unbindService(mServiceConnection);
-			isConnectServer = false;
-		}
-	}
-	
+
+    private static final String TAG = AidlClientActivity.class.getSimpleName();
+    private TextView tevBookName;
+    private TextView tevBookContent;
+    private RecyclerView rcvBook;
+    private TextView tevAddBook;
+
+    //由AIDL文件生成的Java类
+    private BookManager mBookManager = null;
+
+    //标志当前与服务端连接状况的布尔值，false为未连接，true为连接中
+    private boolean isConnectServer = false;
+
+    //包含Book对象的list
+    private List<Book> mBookList = new ArrayList<>();
+    private int num;
+
+    private LinearLayoutManager mLayoutManager;
+    private BookAdapter mBookAdapter;
+
+    @Override
+    protected int initLayoutId() {
+        return R.layout.activity_aidl_client;
+    }
+
+    @Override
+    protected void initData() {
+        num = 0;
+    }
+
+    @Override
+    protected void initViews() {
+        tevBookName = (TextView) findViewById(R.id.tev_book_name);
+        tevBookContent = (TextView) findViewById(R.id.tev_book_content);
+        rcvBook = (RecyclerView) findViewById(R.id.rcv_book);
+        tevAddBook = (TextView) findViewById(R.id.tev_add_book);
+
+        tevAddBook.setOnClickListener(view -> {
+
+            initAddBook();
+        });
+
+        initAdapter();
+    }
+
+    private void initAdapter() {
+        mLayoutManager = new LinearLayoutManager(rxAppCompatActivity);
+        mLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        rcvBook.setLayoutManager(mLayoutManager);
+        rcvBook.setItemAnimator(new DefaultItemAnimator());
+
+        mBookAdapter = new BookAdapter(rxAppCompatActivity);
+        rcvBook.setAdapter(mBookAdapter);
+    }
+
+    private void initAddBook() {
+        //如果与服务端的连接处于未连接状态，则尝试连接
+        if (!isConnectServer) {
+            attemptToBindService();
+//			Toast.makeText(this, "当前与服务端处于未连接状态，正在尝试重连，请稍后再试", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (mBookManager == null) {
+            return;
+        }
+
+        num++;
+        Book book = new Book();
+        book.setName("新书" + num);
+        book.setContent("新书内容" + num);
+        try {
+            mBookManager.addBook(book);
+            mBookList.clear();
+            mBookList.addAll(mBookManager.getBookList());
+            mBookAdapter.clearData();
+            mBookAdapter.addAllData(mBookList);
+            LogManager.i(TAG, "initAddBook*****" + book.toString());
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void initLoadData() {
+
+    }
+
+    /**
+     * 尝试与服务端建立连接
+     */
+    private void attemptToBindService() {
+        Intent intent = new Intent();
+        intent.setAction("com.phone.aidl_client.aidl");
+        intent.setPackage("com.phone.aidl_server");
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            LogManager.i(TAG, "service connected");
+            mBookManager = BookManager.Stub.asInterface(service);
+            isConnectServer = true;
+
+            if (mBookManager != null) {
+                if (mBookList.size() > 0) {
+                    LogManager.i(TAG, "onServiceConnected*****" + mBookList.toString());
+                }
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            LogManager.i(TAG, "service disconnected");
+            isConnectServer = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+//		if (!isConnectServer) {
+//			attemptToBindService();
+//		}
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+//		if (isConnectServer) {
+//			unbindService(mServiceConnection);
+//			isConnectServer = false;
+//		}
+    }
+
 }
