11e9577e772a623e6d7c12159fd22c142ad6b007
“Urasaki” <1164688204@qq.com>
2022-09-17 20:51:16 +0800

    Update

--- base64_and_file/src/main/java/com/phone/base64_and_file/adapter/Base64StrAdapter.java
+++ base64_and_file/src/main/java/com/phone/base64_and_file/adapter/Base64StrAdapter.java
@@ -43,13 +43,13 @@ public class Base64StrAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.item_base64_str, parent, false);
-        return new ContentHolder(view);
+        return new BodyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof ContentHolder) {
-            ContentHolder contentHolder = (ContentHolder) holder;
+        if (holder instanceof BodyHolder) {
+            BodyHolder contentHolder = (BodyHolder) holder;
             contentHolder.tevBase64Str.setText(base64StrList.get(position));
         }
     }
@@ -59,11 +59,11 @@ public class Base64StrAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         return base64StrList.size();
     }
 
-    protected class ContentHolder extends RecyclerView.ViewHolder {
+    protected class BodyHolder extends RecyclerView.ViewHolder {
 
         private TextView tevBase64Str;
 
-        public ContentHolder(@NonNull View itemView) {
+        public BodyHolder(@NonNull View itemView) {
             super(itemView);
 
             tevBase64Str = (TextView) itemView.findViewById(R.id.tev_base64_str);
